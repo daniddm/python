491cd3072cbe93fe7337516b7815163df122f37f
danielddm <daniel.diazdelgado@invesyde.com>
2022-10-28 11:08:24 +0200

    functions

--- day6-11/function.py
+++ day6-11/function.py
@@ -1,5 +1,7 @@
-def greet_with(name,location):
-    print(f"hello {name}")
-    print(f"the location is {location}")
+# def greet_with(name,location):
+#     print(f"hello {name}")
+#     print(f"the location is {location}")
 
-greet_with("Daniel", "Madrid")
+# greet_with("Daniel", "Madrid")
+
+
---
+++ day6-11/function_02.py
@@ -0,0 +1,6 @@
+def nth_root(racands, n):
+    num = racands ** (1/n)
+    return num
+
+
+nth_root(16,2)
