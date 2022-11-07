c1dbcf29b0f7051242ad52a498435eac2d9ae2e4
danielddm <daniel.diazdelgado@invesyde.com>
2022-11-07 16:54:35 +0100

    feat: quiz games

---
+++ day17-21/quiz_game/data.py
@@ -0,0 +1,122 @@
+question_data = [
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which English football club has the nickname &#039;The Foxes&#039;?",
+        "correct_answer": "Leicester City",
+        "incorrect_answers": [
+            "Northampton Town",
+            "Bradford City",
+            "West Bromwich Albion"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "In baseball, how many fouls are an out?",
+        "correct_answer": "0",
+        "incorrect_answers": [
+            "5",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "hard",
+        "question": "Where was the Games of the XXII Olympiad held?",
+        "correct_answer": "Moscow",
+        "incorrect_answers": [
+            "Barcelona",
+            "Tokyo",
+            "Los Angeles"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which city did the former NHL team &quot;The Nordiques&quot; originiate from?",
+        "correct_answer": "Quebec City",
+        "incorrect_answers": [
+            "Houston",
+            "Montreal",
+            "New York"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What is the oldest team in Major League Baseball?",
+        "correct_answer": "Atlanta Braves",
+        "incorrect_answers": [
+            "Chicago Cubs",
+            "Cincinnati Reds",
+            "St. Louis Cardinals"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "What is the exact length of one non-curved part in Lane 1 of an Olympic Track?",
+        "correct_answer": "84.39m",
+        "incorrect_answers": [
+            "100m",
+            "100yd",
+            "109.36yd"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Which player holds the NHL record of 2,857 points?",
+        "correct_answer": "Wayne Gretzky",
+        "incorrect_answers": [
+            "Mario Lemieux ",
+            "Sidney Crosby",
+            "Gordie Howe"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Which professional wrestler fell from the rafters to his death during a live Pay-Per-View event in 1999?",
+        "correct_answer": "Owen Hart",
+        "incorrect_answers": [
+            "Chris Benoit",
+            "Lex Luger",
+            "Al Snow"
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "Who won the UEFA Champions League in 2016?",
+        "correct_answer": "Real Madrid C.F.",
+        "incorrect_answers": [
+            "FC Bayern Munich",
+            "Atletico Madrid",
+            "Manchester City F.C."
+        ]
+    },
+    {
+        "category": "Sports",
+        "type": "multiple",
+        "difficulty": "medium",
+        "question": "Which basketball team has attended the most NBA grand finals?",
+        "correct_answer": "Los Angeles Lakers",
+        "incorrect_answers": [
+            "Boston Celtics",
+            "Philadelphia 76ers",
+            "Golden State Warriors"
+        ]
+    }
+]
