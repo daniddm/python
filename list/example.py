5f908f23772a4e8c1ac3f4e2dcc6660c73e53dcd
danielddm <daniel.diazdelgado@invesyde.com>
2022-11-23 15:54:03 +0100

    feat:examples list

---
+++ list/example.py
@@ -0,0 +1,26 @@
+numbers = [1,2,3]
+new_list = []
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+print(new_list)
+
+
+#list comprehension
+# new_list = [new_item for item in list]
+# list = en que lista se va a iterar
+# item en la lista , lo podemos llamar como queramos
+# new_item es la expresion 
+
+list = [n + 1 for n in numbers]
+print(list)
+
+name = "Daniel"
+n_list = [letter for letter in name]
+print(n_list)
+
+range(1,5)
+
+double_num = [n*2  for n in range(1,5)]
+print(double_num)
